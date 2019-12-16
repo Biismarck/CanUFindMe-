8954f246146695940ca7a74612556be7fc529d5d
Biismarck <l>
2019-12-16 18:16:03 +0800

    修改输出格式

--- CenterNet/src/lib/detectors/ctdet.py
+++ CenterNet/src/lib/detectors/ctdet.py
@@ -96,8 +96,8 @@ class CtdetDetector(BaseDetector):
             for bbox in results[j]:
                 if bbox[4] > self.opt.vis_thresh:
                     debugger.add_coco_bbox(bbox[:4], j - 1, bbox[4], img_id='ctdet')
-                    msg=msg + 'True ' + str(bbox[0]) +' '+ str(bbox[1]) +' '+ str(bbox[2]) +' '+ str(bbox[3])+'\n'
+                    msg=msg + 'True 1 ' + str(bbox[0]) +' '+ str(bbox[1]) +' '+ str(bbox[2]) +' '+ str(bbox[3])+'\n'
         file = open(resultpath +'\\'+ imagename +'.txt', 'w')
         file.write(msg)
         #debugger.show_all_imgs(pause=self.pause)
-        debugger.save_all_imgs(path=resultpath, genID=True)
+        debugger.save_all_imgs(path=resultpath, genID=False)
